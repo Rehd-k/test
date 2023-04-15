6fd01d0f3314280dbfb89f0b4ae9f26b0b57e998
Rhed <coolmirth35@gmail.com>
2023-04-16 00:25:40 +0100

    normal

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('DBCOHScPFVbX1UGgDspIfg==,QFg9JDNyKeVePv75zL9z+A==,4T2cyVLN+3una/lNECatCA==,cO63r4KZZ3/pwCidlsy+mQ=='),
+    keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
